313763d0b83b16add93197993c16b7d0e130b10a
Somsubhra Das <das.somsubhra1@gmail.com>
2021-08-12 01:18:20 +0530

    home get mapping done

--- src/main/java/com/somsubhra/springbootamigoscode/SpringbootAmigosCodeApplication.java
+++ src/main/java/com/somsubhra/springbootamigoscode/SpringbootAmigosCodeApplication.java
@@ -2,12 +2,22 @@ package com.somsubhra.springbootamigoscode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class SpringbootAmigosCodeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootAmigosCodeApplication.class, args);
+    }
+
+    @GetMapping
+    public List<String> hello() {
+        return List.of("hello", "world");
     }
 
 }
