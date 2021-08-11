b24e10c24379a18eaef5b15d25aeeb154d2df3ff
Somsubhra Das <das.somsubhra1@gmail.com>
2021-08-12 01:32:18 +0530

    refactored code in main class

--- src/main/java/com/somsubhra/springbootamigoscode/SpringbootAmigosCodeApplication.java
+++ src/main/java/com/somsubhra/springbootamigoscode/SpringbootAmigosCodeApplication.java
@@ -11,17 +11,10 @@ import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class SpringbootAmigosCodeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootAmigosCodeApplication.class, args);
-    }
-
-    @GetMapping
-    public List<Student> hello() {
-        return List.of(new Student(1L, "Mariam",
-                "mariam@gmail.com", LocalDate.of(2000, Month.MAY, 23), 21));
     }
 
 }
