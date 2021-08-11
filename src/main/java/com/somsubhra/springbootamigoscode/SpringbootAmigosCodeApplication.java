c9843723e7f04b9e43fe5f45ed882368deb283c9
Somsubhra Das <das.somsubhra1@gmail.com>
2021-08-12 01:07:14 +0530

    initial commit

---
+++ src/main/java/com/somsubhra/springbootamigoscode/SpringbootAmigosCodeApplication.java
@@ -0,0 +1,13 @@
+package com.somsubhra.springbootamigoscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAmigosCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootAmigosCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/somsubhra/springbootamigoscode/SpringbootAmigosCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somsubhra.springbootamigoscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAmigosCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
