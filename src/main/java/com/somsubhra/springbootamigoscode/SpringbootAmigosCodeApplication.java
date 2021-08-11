03e5af96b6a0c02b305d5a1b3e1124f7186d4532
Somsubhra Das <das.somsubhra1@gmail.com>
2021-08-12 01:28:25 +0530

    added get api for students

--- src/main/java/com/somsubhra/springbootamigoscode/SpringbootAmigosCodeApplication.java
+++ src/main/java/com/somsubhra/springbootamigoscode/SpringbootAmigosCodeApplication.java
@@ -1,10 +1,13 @@
 package com.somsubhra.springbootamigoscode;
 
+import com.somsubhra.springbootamigoscode.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,8 +19,9 @@ public class SpringbootAmigosCodeApplication {
     }
 
     @GetMapping
-    public List<String> hello() {
-        return List.of("hello", "world");
+    public List<Student> hello() {
+        return List.of(new Student(1L, "Mariam",
+                "mariam@gmail.com", LocalDate.of(2000, Month.MAY, 23), 21));
     }
 
 }
