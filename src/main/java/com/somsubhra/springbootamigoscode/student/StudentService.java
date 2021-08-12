159a4f79c74737b79a9f3c39372b84323b0a6d84
Somsubhra Das <das.somsubhra1@gmail.com>
2021-08-12 13:03:09 +0530

    implemented Student repository in Student service

--- src/main/java/com/somsubhra/springbootamigoscode/student/StudentService.java
+++ src/main/java/com/somsubhra/springbootamigoscode/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.somsubhra.springbootamigoscode.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -8,8 +9,17 @@ import java.util.List;
 
 @Service // more specific to service. Can even use Component here.
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(new Student(1L, "Mariam",
-                "mariam@gmail.com", LocalDate.of(2000, Month.MAY, 23), 21));
+//        return List.of(new Student(1L, "Mariam",
+//                "mariam@gmail.com", LocalDate.of(2000, Month.MAY, 23), 21));
+        return studentRepository.findAll();
     }
 }
