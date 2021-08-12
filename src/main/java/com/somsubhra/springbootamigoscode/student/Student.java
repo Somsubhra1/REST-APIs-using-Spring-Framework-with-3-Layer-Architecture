ef94f0b364f0128b4ebd3a63dde2f9b6c9b3e290
Somsubhra Das <das.somsubhra1@gmail.com>
2021-08-12 12:52:25 +0530

    mapped tables, entities and ids with jpa and table

--- src/main/java/com/somsubhra/springbootamigoscode/student/Student.java
+++ src/main/java/com/somsubhra/springbootamigoscode/student/Student.java
@@ -1,8 +1,21 @@
 package com.somsubhra.springbootamigoscode.student;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity // used in writing queries
+@Table // used to name table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private long id;
     private String name;
     private String email;
