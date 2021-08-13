26fad2665aae7a3e0a290f2d007b0ea2e80cbd19
Somsubhra Das <das.somsubhra1@gmail.com>
2021-08-13 11:48:34 +0530

    added student config to add students to db

---
+++ src/main/java/com/somsubhra/springbootamigoscode/student/StudentConfig.java
@@ -0,0 +1,32 @@
+package com.somsubhra.springbootamigoscode.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student mariam = new Student(
+                    "Mariam",
+                    "mariam@gmail.com",
+                    LocalDate.of(2000, Month.MAY, 23),
+                    21
+            );
+            Student alex = new Student(
+                    "alex",
+                    "alex@gmail.com",
+                    LocalDate.of(2004, Month.MAY, 23),
+                    21
+            );
+
+            repository.saveAll(List.of(mariam, alex));
+        };
+    }
+}
